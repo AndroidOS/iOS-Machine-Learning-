f410dca2b67803e30b92f6cb8605e1cd674aed8b
AndroidOS <burrowsgit@gmail.com>
2020-09-09 06:32:48 +1000

    Added test image from Image Picker

--- SeeBooks/ViewController.swift
+++ SeeBooks/ViewController.swift
@@ -4,18 +4,34 @@
 //
 //  Created by Manuel Carvalho on 5/9/20.
 //  Copyright © 2020 Manuel Carvalho. All rights reserved.
-//
+//  Bases on Angela Yu's code
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
     
+    @IBOutlet weak var imageView: UIImageView!
     let imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
       
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+                   
+                   imageView.image = image
+                   imagePicker.dismiss(animated: true, completion: nil)
+                   guard let ciImage = CIImage(image: image) else {
+                       fatalError("couldn't convert uiimage to CIImage")
+                   }
+                   
+               }
+        
     }
 
     @IBAction func btnGetPhoto(_ sender: UIBarButtonItem) {
@@ -24,5 +40,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         present(imagePicker, animated: true, completion: nil)
     }
     
+
 }
 
